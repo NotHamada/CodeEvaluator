823cfa153ea6b991dc15753d1402ec6b1c529e5b
Matheus <matheushamada@live.com>
2024-01-03 18:28:34 -0300

    Identificando variáveis

--- Eval.rkt
+++ Eval.rkt
@@ -38,20 +38,21 @@
 
 (define (calculate lst)
   (define grade 0)
-  (define lines (car (cdddr lst)))
+  (define comments (car lst))
+  (define lines (car (cdr lst)))
   (define functions (car (cddr lst)))
-  (define average (exact->inexact (/ lines functions)))
+  (define sum-function-lines (car (cdddr lst)))
+  (define variables (car (cddddr lst)))
+  (define average (exact->inexact (/ sum-function-lines functions)))
 
   (displayln (format "Average lines per function: ~a" average))
+  (displayln (format "Variables: ~a" variables))
 
   (cond
     [(< average 5) (displayln "Código ruim")]
-    [(and (> average 5) (< average 10)) (displayln "Trop do código médio")]
+    [(and (> average 5) (< average 10)) (displayln "Tropa do código médio")]
     )
 
-  (displayln (car (cdr lst)))
-  (displayln lines)
-  (displayln functions)
 )
 
 ; Exemplo de acesso aos itens da lista: (car lst) acessa o primeiro item
@@ -69,28 +70,32 @@
              (comments 0)
              (number-of-functions 0)
              (sum-function-lines 0)
+             (variables 0)
              (line (read-line input-port)))
     (cond
       [(eof-object? line)
        (close-input-port input-port)
-       (calculate (list comments line-number number-of-functions sum-function-lines))
+       (calculate (list comments line-number number-of-functions sum-function-lines variables))
        (exit)
        ]
       [(or (is-empty-line? line) (= 1 line-number))
-       (loop (add1 line-number) function-lines open-parentesis close-parentesis comments number-of-functions sum-function-lines (read-line input-port))]
+       (loop (add1 line-number) function-lines open-parentesis close-parentesis comments number-of-functions sum-function-lines variables (read-line input-port))]
       [(= 1 (verify-comment line)) 
-       (loop (add1 line-number) function-lines open-parentesis close-parentesis (add1 comments) number-of-functions sum-function-lines (read-line input-port))]
+       (loop (add1 line-number) function-lines open-parentesis close-parentesis (add1 comments) number-of-functions sum-function-lines variables (read-line input-port))]
       [else
        (let* (
               (new-open-parentesis (+ open-parentesis (count-open-parentesis line)))
               (new-close-parentesis (+ close-parentesis (count-close-parentesis line)))
               )
+         (when (and (= new-open-parentesis new-close-parentesis) (= new-close-parentesis 1) (= function-lines 0))
+           (loop (add1 line-number) 0 0 0 comments number-of-functions (+ sum-function-lines function-lines) (add1 variables) (read-line input-port)) 
+           )
          (when (and (= new-open-parentesis new-close-parentesis) (< 0 new-open-parentesis) (< 0 new-close-parentesis))
            ; Achar um jeito de identificar variavel: quando open-parentesis é 1 e close-parentesis é 1 
            (displayln (format "Line ~a: Function Lines ~a" line-number (if (zero? function-lines) 1 function-lines)))
-           (loop (add1 line-number) 0 0 0 comments (add1 number-of-functions) (+ sum-function-lines function-lines) (read-line input-port))
+           (loop (add1 line-number) 0 0 0 comments (add1 number-of-functions) (+ sum-function-lines function-lines) variables (read-line input-port))
            )
-         (loop (add1 line-number) (+ function-lines 1) new-open-parentesis new-close-parentesis comments number-of-functions sum-function-lines (read-line input-port)))])))
+         (loop (add1 line-number) (+ function-lines 1) new-open-parentesis new-close-parentesis comments number-of-functions sum-function-lines variables (read-line input-port)))])))
 
 ; Example usage
 (define file-path "teste.rkt")
--- teste.rkt
+++ teste.rkt
@@ -1,5 +1,11 @@
 #lang racket
 
+(define testa 1)
+(define teste 1)
+(define testi 1)
+(define testo 1)
+(define testu 1)
+
 (define (trim-whitespace str)
   (define (is-whitespace? c)
     (char-whitespace? c))
